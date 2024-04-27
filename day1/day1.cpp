2833f74d198d92b7d17cbf61c5ec7da4f69064ab
Daksh7112003 <7daksh2003@gmail.com>
2024-04-27 00:06:11 -0700

    day1

---
+++ day1/day1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int a =2 ;
+    // int b =3;
+
+    int a=2;int b=3;
+
+
+
+
+
+
+
+
+
+
+    float pi = 3.14;
+
+
+    char hello = 'p';
+
+
+
+
+    cout<<"helloworld"<<a<<b<< pi<<" "<<hello;
+    return 0;
+    
+}

ee4839d8fe29cf20def6e89b49c26a9de94d8130
Daksh7112003 <7daksh2003@gmail.com>
2024-04-27 02:42:35 -0700

    day5

---
+++ day5/day5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int a = 45 ;
+    float b  =  45.56;
+
+
+    cout<<"The value of b is "<<float(a)<<endl;
+
+cout<<"The value of b is "<<(float)a<<endl;
+
+cout<<"The expression is "<<a + b<<endl;
+
+cout<<"The expression is "<<a + int(b)<<endl;
+
+cout<<"The expression is "<<a + (int)(b)<<endl;
+return 0 ; 
+}

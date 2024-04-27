9f9ae8bb9e93f4fc6a0e5cd44fa0cba9e48fbd03
Daksh7112003 <7daksh2003@gmail.com>
2024-04-27 02:21:22 -0700

    day4

---
+++ day4/day4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+
+// int main(){
+//     float d = 234.45f;  //reference variable
+
+
+//     long double e = 325.44l;
+
+   
+
+
+
+//     cout<<"Thee value of d is "<<d<<endl<<"The value of e is "<<e;
+
+
+
+    
+// }
+
+
+
+int main(){
+
+
+
+    float d = 34.4F;
+    long double e =34.5L;
+    cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
+   
+    cout<<"The size of 34.4 is "<< sizeof(34.4F)<<endl;
+    cout<<"The size of 34.4 is "<< sizeof(34.4f)<<endl;
+    cout<<"The size of 34.4 is "<< sizeof(34.4L)<<endl;
+    cout<<"The size of 34.4 is "<< sizeof(34.4l)<<endl;
+
+}

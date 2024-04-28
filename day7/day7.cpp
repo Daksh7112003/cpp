de3025e7a97b5987ab7f05f2bb1bcf3ff72b0a12
Daksh7112003 <7daksh2003@gmail.com>
2024-04-27 23:56:23 -0700

    day7

---
+++ day7/day7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+    //for loop in c++
+
+
+
+    // for (int i = 0; i < 100; i++) {
+    //     cout << i << endl;
+    // }
+
+
+    // while loop in c++
+
+  int   i=1;
+
+
+while(i<100){
+cout<<i<<endl;
+
+i++;
+
+}
+
+
+
+    return 0;
+}

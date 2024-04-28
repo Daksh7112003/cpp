7f550435ab17000abbe5255fef8ed838f48e86d8
Daksh7112003 <7daksh2003@gmail.com>
2024-04-28 00:50:22 -0700

    day9

---
+++ day9/day9.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a =3;
+    int*b = &a;
+
+    cout<<"Address of a is "<< &a<<endl;
+
+
+    cout<<"Address of b is "<<b<<endl;
+
+
+
+
+    // & -----> address of operator ...
+
+
+    //* ----> Dereference operator ...
+
+
+
+
+cout<<"The value at address b is"<<*b<<endl;
+
+
+//* this will print the address of the variable ....
+
+
+
+
+    return 0;
+
+}

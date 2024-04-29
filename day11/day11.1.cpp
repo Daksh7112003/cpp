53b685e2f2ee3243e698555474b3a68160fe1078
Daksh7112003 <7daksh2003@gmail.com>
2024-04-29 00:10:50 -0700

    day11

---
+++ day11/day11.1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[]={11,22,33};
+    for(int i= 0 ; i<3;i++){
+        cout<<marks[i]<<endl;
+
+    }
+
+   int * p =marks;
+
+
+
+
+
+    cout<<"the value of *p is "<<*p<<endl;
+    cout<<"The value of *(p+1) is"<<*(p+1)<<endl;
+
+    cout<<"The value of *(p+2) is "<<*(p+2)<<endl;
+     return 0;
+     
+
+
+}

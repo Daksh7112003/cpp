b25d1918690cea4cb870e95ccf11b28529bed503
Daksh7112003 <7daksh2003@gmail.com>
2024-05-03 00:55:54 -0700

    day23

---
+++ day23/day23.cpp
@@ -0,0 +1,144 @@
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
---
+++ day23/day23b.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std ;
+
+class point{
+
+
+int x, y ;
+
+
+public:
+point(int a , int b){
+
+    x =a ;
+    y=b ;
+
+}
+
+
+
+ void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+
+
+
+
+};
+
+int main(){
+
+
+
+    point p(1,4);
+
+    p.displayPoint();
+    
+
+
+}
+
+
+

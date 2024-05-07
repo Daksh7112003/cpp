a5c444859d487d5e16c343d13367bd6c1e35a53a
Daksh7112003 <7daksh2003@gmail.com>
2024-05-07 01:03:13 -0700

    day27

---
+++ day27/day27.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std ;
+
+
+
+
+
+
+
+
+
+
+class base{
+
+    protected:
+
+    int a ;
+
+
+    public:
+
+    int d ; 
+
+
+
+
+
+    
+
+};
+
+
+
+class derived:protected base{
+
+
+
+
+
+};
+
+
+
+
+
+int main(){
+
+    base b ;
+    derived d;
+    cout<<d.a; //// note this will not work here..... as its protected so you cant access that class member ....
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+    return  0 ; 
+
+}
---
+++ day28/day28.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+protected:
+    int roll_number;
+
+public:
+    void set_roll_number(int);
+    void get_roll_number(void);
+};
+
+void Student ::set_roll_number(int r)
+{
+    roll_number = r;
+}
+
+void Student ::get_roll_number()
+{
+    cout << "The roll number is " << roll_number << endl;
+}
+
+
+
+int main(){
+
+
+    return 0 ;
+
+}

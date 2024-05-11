51a8678806e3056c632be5b50c0809c94d4f7b54
Daksh7112003 <7daksh2003@gmail.com>
2024-05-11 03:35:37 -0700

    day36

---
+++ day36/day36.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits./stdc++.h>
+
+using namespace std;
+
+
+//pairs ..
+
+int main(){
+
+    //name , age , weight ....
+
+    pair<pair<string,int>,int>p;
+    p.first.first="rohit";
+    p.first.second= 45;
+
+    p.second= 30;
+
+    cout<<p.first.first<<" " <<p.second<<" "<<p.first.second<<endl;
+
+
+
+}
